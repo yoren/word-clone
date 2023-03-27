49d1bd0981088118532476b23701dd5ab1b5c684
Yoren Chang <yorenchang@gmail.com>
2023-03-27 21:50:44 +0800

    Changes for exercise 1

---
+++ src/components/Form/Form.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Form({guess, setGuess}) {
+  const [ guessInput, setGuessInput ] = React.useState( "" );
+
+  const onSubmitHandler = ( event ) => {
+    event.preventDefault();
+
+    setGuess( guessInput );
+    setGuessInput('');
+  };
+
+  return <form className="guess-input-wrapper" onSubmit={ onSubmitHandler }>
+    <label htmlFor="guess-input">Enter guess:</label>
+    <input id="guess-input" type="text" pattern="[A-Za-z]{5}" value={ guessInput } onChange={ ( e ) => { setGuessInput( e.target.value.toUpperCase() ); } } />
+  </form>;
+}
+
+export default Form;
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import Form from '../Form';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +10,15 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [ guess, setGuess ] = React.useState( "" );
+
+  React.useEffect(() => {
+    if ( guess ) {
+      console.info({ guess });
+    }
+  }, [guess]);
+
+  return <Form guess={guess} setGuess={setGuess} />;
 }
 
 export default Game;
